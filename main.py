f7a9ebd2b3ce0870b4ee04e50ce56446f47ebe56
Slashbeetle <>
2021-04-23 16:38:38 +0000

    bot init

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+from Keepalive import keep_alive
+
+from telegram import*
+from telegram.ext import*
+
+my_secret = os.environ['TOKEN']
+
+#bot = Bot("1779113707:AAH5b50y7Qo-aRcFmq094KiQ_VEJJNERcrw")
+bot = Bot(my_secret)
+
+print(bot.get_me())
+updater = Updater(my_secret,use_context="true")
+
+dispatcher= updater.dispatcher
+
+
+def spamfn0(update:Update,context:CallbackContext):
+  bot.send_message(
+      chat_id=update.effective_chat.id,
+      text= "hi hello world",
+  )
+
+start_value= CommandHandler('spam',spamfn0)
+dispatcher.add_handler(start_value)
+updater.start_polling()
+
+def spamfn1(update:Update,context:CallbackContext):
+  bot.send_message(
+      chat_id=update.effective_chat.id,
+      text= "hi hello world welcome",
+  )
+
+start_value= CommandHandler('spam1',spamfn1)
+dispatcher.add_handler(start_value)
+updater.start_polling()
+
+keep_alive()
+token = os.environ.get("token")
+#bot.run(token)
